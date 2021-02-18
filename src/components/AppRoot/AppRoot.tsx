bffa20a40c24b09d5eb9b412920a4b2710df692b
Vladimir Klepov <v.klepov@gmail.com>
2021-02-18 11:32:02 +0300

    feat: Control legacy classes via AppRoot

--- src/components/AppRoot/AppRoot.tsx
+++ src/components/AppRoot/AppRoot.tsx
@@ -4,6 +4,7 @@ import { classNames } from '../../lib/classNames';
 import { AppRootContext } from './AppRootContext';
 import { withAdaptivity, SizeType, AdaptivityProps } from '../../hoc/withAdaptivity';
 import { useIsomorphicLayoutEffect } from '../../lib/useIsomorphicLayoutEffect';
+import { classScopingMode } from '../../lib/classScopingMode';
 
 // Используйте classList, но будьте осторожны
 /* eslint-disable no-restricted-properties */
@@ -11,6 +12,8 @@ import { useIsomorphicLayoutEffect } from '../../lib/useIsomorphicLayoutEffect';
 export interface AppRootProps extends HTMLAttributes<HTMLDivElement>, AdaptivityProps {
   embedded?: boolean;
   window?: Window;
+  /** Добавляет на элементы классы без префикса (.Button) для обратной совместимости */
+  noConflictClass?: boolean;
 }
 
 function applyAdaptivityStyles(container: HTMLElement, sizeX: SizeType) {
@@ -21,15 +24,18 @@ function applyAdaptivityStyles(container: HTMLElement, sizeX: SizeType) {
   }
 }
 
-const AppRoot: FC<AppRootProps> = ({ children, embedded, sizeX, hasMouse }) => {
+const AppRoot: FC<AppRootProps> = ({ children, embedded, sizeX, hasMouse, noConflictClass = false }) => {
   const rootRef = useRef<HTMLDivElement>();
   const [portalRoot, setPortalRoot] = useState<HTMLDivElement>(null);
   const { window } = useDOM();
 
   const initialized = useRef(false);
 
-  if (window && !initialized.current && !embedded) {
-    window.document.documentElement.classList.add('vkui');
+  if (!initialized.current) {
+    if (window && !embedded) {
+      window.document.documentElement.classList.add('vkui');
+    }
+    classScopingMode.noConflict = noConflictClass;
   }
 
   // one time initialization and cleanup
